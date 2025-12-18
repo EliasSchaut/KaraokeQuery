11f03488171d8e284eacd0e3ae10a89e19dc5a5a
EliasSchaut <eschaut@outlook.de>
2025-12-18 15:13:57 +0100

    add loading spinner

--- app/pages/[...home].vue
+++ app/pages/[...home].vue
@@ -9,23 +9,36 @@
         class="even:bg-second-200 dark:even:bg-second-800 dark:odd:bg-second-900 odd:bg-second-100 not-first:border-t-second-300 dark:not-first:border-t-second-700 p-3 not-first:border-t"
       />
     </template>
-    <div v-if="!result?.hits?.length" class="text-center">No tracks found</div>
+    <Spinner
+      v-if="searching"
+      class="text-prime-500 mx-auto flex items-center justify-center"
+    />
+    <div v-else-if="!result?.hits?.length" class="text-center">
+      No tracks found
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import Spinner from '~/components/spinner.vue';
+
 export default defineComponent({
+  components: { Spinner },
   setup() {
     const { search, result } = useMeiliSearch('karaoke');
 
     return {
       search,
       result,
+      searching: ref(false),
     };
   },
   methods: {
     async on_query(query: string) {
+      this.searching = true;
+      if (this.result?.hits) this.result.hits = [];
       await this.search(query);
+      this.searching = false;
     },
   },
 });
