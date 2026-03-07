e14392f4c97a10b23f1117dfa101601d29431198
EliasSchaut <elias@schaut.dev>
2026-03-07 15:13:50 +0100

    fix metadata

--- app/app.vue
+++ app/app.vue
@@ -1,32 +1,37 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-  <Title>KIT GameJam</Title>
-  <Meta name="description" content="KIT GameJam - Create. Meet. Play." />
+  <Title>KaraokeQuery</Title>
   <Meta
-      name="keywords"
-      content="gamejam, gamedev, Hochschulgruppe, KIT, Karlsruhe"
+    name="description"
+    content="A simple web frontend for meilisearch that disblays available karaoke songs downloaded with usdb_syncer"
   />
-  <Meta name="og:title" content="KIT GameJam" />
-  <Meta name="og:description" content="KIT GameJam - Create. Meet. Play." />
+  <Meta
+    name="keywords"
+    content="Karaoke,Songs,List,Query,MelodyMania,Ultrastar"
+  />
+  <Meta name="og:title" content="KaraokeQuery" />
+  <Meta
+    name="og:description"
+    content="A simple web frontend for meilisearch that disblays available karaoke songs downloaded with usdb_syncer"
+  />
   <Meta name="og:image" content="/og.png" />
   <Meta name="theme-color" content="#8b5cf6" />
   <Link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicon/apple-touch-icon.png"
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/favicon/apple-touch-icon.png"
   />
   <Link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicon/favicon-32x32.png"
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicon/favicon-32x32.png"
   />
   <Link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicon/favicon-16x16.png"
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicon/favicon-16x16.png"
   />
   <Link rel="icon" href="/favicon/favicon.ico" />
   <Link rel="manifest" href="/site.webmanifest" />
